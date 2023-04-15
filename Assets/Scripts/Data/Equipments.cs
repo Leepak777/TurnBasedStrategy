dc23aeda503d0638c8c284b9df6ce2085c90e680
Leepak777 <LPSA732@gmail.com>
2023-04-14 18:48:51 -0700

    setting up ingame equipment and type editor

--- Assets/Scripts/Data/Equipments.cs
+++ Assets/Scripts/Data/Equipments.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Equipments", order = 1)]
 public class Equipments : ScriptableObject
 {
+
     public List<string> weapon = new List<string>(){"none","Light Glaive", "Gladius", "Power Sword", "Great Sword", "Pike","Mace","Pistol","Rifle"};
     public List<string> armor = new List<string>(){"none","Plate","Half Plate","Synthe Armor","Legionary Armor","Praetorian_Armor","Flak Suit","Assault Vest","Personal Shield MK I","Personal Shield MK II","Personal Shield MK III"};
     public List<string> shield = new List<string>(){"none","Kite Shield","Tower Shield"};
@@ -25,64 +26,124 @@ public class Equipments : ScriptableObject
     10. #of attacks
     */
     //weapon
-    UDictionary<string,float> Light_Glaive = new UDictionary<string, float>(){
-        {"wd",6},{"pscal",1.3f},{"dscal",0.5f},{"ap",10},{"sp",90},
-        {"rng",2},{"acc",1},{"mdb",-2},{"rdb",-1},{"w_enc",3},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Gladius = new UDictionary<string, float>(){
-        {"wd",5},{"pscal",1},{"dscal",0.5f},{"ap",25},{"sp",100},
-        {"rng",1},{"acc",3},{"mdb",0},{"rdb",0},{"w_enc",2},
-        {"attack_num",2}
-    };
-    UDictionary<string,float> Power_Sword = new UDictionary<string, float>(){
-        {"wd",8},{"pscal",1},{"dscal",1},{"ap",0},{"sp",100},
-        {"rng",1.5f},{"acc",2},{"mdb",1},{"rdb",0},{"w_enc",3},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Great_Sword = new UDictionary<string, float>(){
-        {"wd",9},{"pscal",1.3f},{"dscal",1},{"ap",0},{"sp",100},
-        {"rng",1.5f},{"acc",2},{"mdb",1},{"rdb",0},{"w_enc",3},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Pike = new UDictionary<string, float>(){
-        {"wd",7},{"pscal",1.5f},{"dscal",0.2f},{"ap",25},{"sp",100},
-        {"rng",2.3f},{"acc",1},{"mdb",-1},{"rdb",-1},{"w_enc",3},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Mace = new UDictionary<string, float>(){
-        {"wd",10},{"pscal",2},{"dscal",0},{"ap",50},{"sp",50},
-        {"rng",1.5f},{"acc",2},{"mdb",-2},{"rdb",-1},{"w_enc",4},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Pistol = new UDictionary<string, float>(){
-        {"wd",12},{"pscal",0},{"dscal",1},{"ap",50},{"sp",0},
-        {"rng",8},{"acc",2},{"mdb",0},{"rdb",0},{"w_enc",4},
-        {"attack_num",1}
-    };
-    UDictionary<string,float> Rifle = new UDictionary<string, float>(){
-        {"wd",16},{"pscal",0},{"dscal",1},{"ap",50},{"sp",0},
-        {"rng",8},{"acc",2},{"mdb",0},{"rdb",0},{"w_enc",4},
-        {"attack_num",1}
-    };
+    
+    void Awake(){
+        Light_Glaive = new UDictionary<string, float>(){
+            {"wd",6},{"pscal",1.3f},{"dscal",0.5f},{"ap",10},{"sp",90},
+            {"rng",2},{"acc",1},{"mdb",-2},{"rdb",-1},{"w_enc",3},
+            {"attack_num",1}
+        };
+        Gladius = new UDictionary<string, float>(){
+            {"wd",5},{"pscal",1},{"dscal",0.5f},{"ap",25},{"sp",100},
+            {"rng",1},{"acc",3},{"mdb",0},{"rdb",0},{"w_enc",2},
+            {"attack_num",2}
+        };
+        Power_Sword = new UDictionary<string, float>(){
+            {"wd",8},{"pscal",1},{"dscal",1},{"ap",0},{"sp",100},
+            {"rng",1.5f},{"acc",2},{"mdb",1},{"rdb",0},{"w_enc",3},
+            {"attack_num",1}
+        };
+        Great_Sword = new UDictionary<string, float>(){
+            {"wd",9},{"pscal",1.3f},{"dscal",1},{"ap",0},{"sp",100},
+            {"rng",1.5f},{"acc",2},{"mdb",1},{"rdb",0},{"w_enc",3},
+            {"attack_num",1}
+        };
+        Pike = new UDictionary<string, float>(){
+            {"wd",7},{"pscal",1.5f},{"dscal",0.2f},{"ap",25},{"sp",100},
+            {"rng",2.3f},{"acc",1},{"mdb",-1},{"rdb",-1},{"w_enc",3},
+            {"attack_num",1}
+        };
+        Mace = new UDictionary<string, float>(){
+            {"wd",10},{"pscal",2},{"dscal",0},{"ap",50},{"sp",50},
+            {"rng",1.5f},{"acc",2},{"mdb",-2},{"rdb",-1},{"w_enc",4},
+            {"attack_num",1}
+        };
+        Pistol = new UDictionary<string, float>(){
+            {"wd",12},{"pscal",0},{"dscal",1},{"ap",50},{"sp",0},
+            {"rng",8},{"acc",2},{"mdb",0},{"rdb",0},{"w_enc",4},
+            {"attack_num",1}
+        };
+        Rifle = new UDictionary<string, float>(){
+            {"wd",16},{"pscal",0},{"dscal",1},{"ap",50},{"sp",0},
+            {"rng",8},{"acc",2},{"mdb",0},{"rdb",0},{"w_enc",4},
+            {"attack_num",1}
+        };
+        Pack_Horse = new UDictionary<string, float>(){
+            {"base_hp",30},{"mdb",3},{"eq_mov",8},{"eq_init",+5},{"eq_enc",-6}
+        };
+        War_Horse = new UDictionary<string, float>(){
+            {"base_hp",35},{"mdb",4},{"eq_mov",9},{"eq_init",+6},{"eq_enc",-4}
+        };
+        Plate = new UDictionary<string, float>(){
+            {"av",16},{"sv",0},{"mdb",-2},{"rdb",-2},{"eq_mov",-1},
+            {"eq_init",-3},{"eq_enc",6}
+        };
+        Half_Plate = new UDictionary<string, float>(){
+            {"av",14},{"sv",0},{"mdb",-1},{"rdb",-1},{"eq_mov",-1},
+            {"eq_init",-1},{"eq_enc",4}
+        };
+        Synthe_Armor = new UDictionary<string, float>(){
+            {"av",13},{"sv",0},{"mdb",0},{"rdb",0},{"eq_mov",0},
+            {"eq_init",0},{"eq_enc",2}
+        };
+        Legionary_Armor = new UDictionary<string, float>(){
+            {"av",15},{"sv",0},{"mdb",-1},{"rdb",-2},{"eq_mov",0},
+            {"eq_init",-1},{"eq_enc",4}
+        };
+        Praetorian_Armor = new UDictionary<string, float>(){
+            {"av",18},{"sv",0},{"mdb",1},{"rdb",-3},{"eq_mov",-1},
+            {"eq_init",-3},{"eq_enc",7}
+        };
+        Flak_Suit = new UDictionary<string, float>(){
+            {"av",13},{"sv",0},{"mdb",-1},{"rdb",0},{"eq_mov",0},
+            {"eq_init",0},{"eq_enc",2}
+        };
+        Assault_Vest = new UDictionary<string, float>(){
+            {"av",16},{"sv",0},{"mdb",0},{"rdb",-1},{"eq_mov",-1},
+            {"eq_init",-2},{"eq_enc",4}
+        };
+        Personal_Shield_MK_I = new UDictionary<string, float>(){
+            {"av",0},{"sv",15},{"mdb",0},{"rdb",0},{"eq_mov",0},
+            {"eq_init",0},{"eq_enc",1}
+        };
+        Personal_Shield_MK_II = new UDictionary<string, float>(){
+            {"av",0},{"sv",17},{"mdb",0},{"rdb",0},{"eq_mov",0},
+            {"eq_init",0},{"eq_enc",1}
+        };
+        Personal_Shield_MK_III = new UDictionary<string, float>(){
+            {"av",0},{"sv",19},{"mdb",-1},{"rdb",-1},{"eq_mov",0},
+            {"eq_init",-1},{"eq_enc",2}
+        };
+        Buckler = new UDictionary<string, float>(){
+            {"pr",2},{"pv",19},{"mdb",0},{"rdb",0},{"acc",0},{"eq_enc",1}
+        };
+        Buckler2 = new UDictionary<string, float>(){
+        {"pr",2},{"pv",19},{"mdb",-1},{"rdb",-2},{"acc",0},{"eq_enc",2}
+        };
+        Kite_Shield = new UDictionary<string, float>(){
+            {"pr",4},{"pv",15},{"mdb",-2},{"rdb",-2},{"eq_init",-2},{"eq_enc",2}
+        };
+        Tower_Shield = new UDictionary<string, float>(){
+        {"pr",6},{"pv",16},{"mdb",-2},{"rdb",-2},{"eq_init",-1},{"eq_enc",-3}
+        };
+        Weapon_lst = new UDictionary<string, UDictionary<string, float>>(){
+            {"Light Glaive",Light_Glaive},{"Gladius",Gladius},{"Power Sword",Power_Sword},{"Great Sword",Great_Sword},{"Pike",Pike},{"Mace",Mace},{"Pistol",Pistol},{"Rifle",Rifle}
+        };
+        Buckler_lst = new UDictionary<string, UDictionary<string, float>>(){
+            {"Buckler",Buckler},{"Buckler2",Buckler2}
+        };   
+        Armor_lst = new UDictionary<string, UDictionary<string, float>>(){
+            {"Plate",Plate}, {"Half Plate",Half_Plate}, {"Synthe Armor",Synthe_Armor}, {"Legionary Armor",Legionary_Armor}, {"Praetorian Armor",Praetorian_Armor}, {"Flak Suit",Flak_Suit},{"Assault Vest",Assault_Vest}, {"Personal Shield MK I",Personal_Shield_MK_I},{"Personal Shield MK II",Personal_Shield_MK_II},{"Personal Shield MK III",Personal_Shield_MK_III}
+        };    
+        Mount_lst = new UDictionary<string, UDictionary<string, float>>(){
+            {"Pack Horse",Pack_Horse},{"War Horse",War_Horse}
+        };         
+    }
+    UDictionary<string,float> Light_Glaive,Gladius,Power_Sword,Great_Sword,Pike,Mace,Pistol,Rifle = new UDictionary<string,float>();
+    UDictionary<string,UDictionary<string,float>> Weapon_lst = new UDictionary<string, UDictionary<string, float>>();    
     public UDictionary<string,float> getWeaponStat(string weapon){
-        switch(weapon){
-            case "Light Glaive":
-                return Light_Glaive;
-            case "Gladius":
-                return Gladius;
-            case "Power Sword":
-                return Power_Sword;
-            case "Great Sword":
-                return Great_Sword;
-            case "Pike":
-                return Pike;
-            case "Mace":
-                return Mace;
-            case "Pistol":
-                return Pistol;
-            case "Rifle":
-                return Rifle;
+        if(Weapon_lst.ContainsKey(weapon)){
+            return Weapon_lst[weapon];
         }
         return null;
     }
@@ -95,18 +156,11 @@ public class Equipments : ScriptableObject
     4. init
     5. enc
     */
-    UDictionary<string,float> Kite_Shield = new UDictionary<string, float>(){
-        {"pr",4},{"pv",15},{"mdb",-2},{"rdb",-2},{"eq_init",-2},{"eq_enc",2}
-    };
-    UDictionary<string,float> Tower_Shield = new UDictionary<string, float>(){
-       {"pr",6},{"pv",16},{"mdb",-2},{"rdb",-2},{"eq_init",-1},{"eq_enc",-3}
-    };
+    UDictionary<string,float> Kite_Shield,Tower_Shield= new UDictionary<string,float>();
+    UDictionary<string,UDictionary<string,float>> Shield_lst = new UDictionary<string, UDictionary<string, float>>();    
     public UDictionary<string,float> getShieldStat(string shield){
-        switch(shield){
-            case "Kite Shield":
-                return Kite_Shield;
-            case "Tower Shield":
-                return Tower_Shield;
+        if(Shield_lst.ContainsKey(shield)){
+            return Shield_lst[shield];
         }
         return null;
 
@@ -120,18 +174,11 @@ public class Equipments : ScriptableObject
     4. rdb
     5. enc
     */
-    UDictionary<string,float> Buckler = new UDictionary<string, float>(){
-        {"pr",2},{"pv",19},{"mdb",0},{"rdb",0},{"acc",0},{"eq_enc",1}
-    };
-    UDictionary<string,float> Buckler2 = new UDictionary<string, float>(){
-       {"pr",2},{"pv",19},{"mdb",-1},{"rdb",-2},{"acc",0},{"eq_enc",2}
-    };
+    UDictionary<string,float> Buckler,Buckler2= new UDictionary<string,float>();
+    UDictionary<string,UDictionary<string,float>> Buckler_lst = new UDictionary<string, UDictionary<string, float>>();   
     public UDictionary<string,float> getBucklerStat(string buckler){    
-        switch(buckler){
-            case "Buckler":
-                return Buckler;
-            case "Buckler2":
-                return Buckler2;
+        if(Buckler_lst.ContainsKey(buckler)){
+            return Buckler_lst[buckler];
         }
         return null;
 
@@ -147,68 +194,12 @@ public class Equipments : ScriptableObject
     5. init
     6. enc
     */
-    UDictionary<string,float> Plate = new UDictionary<string, float>(){
-        {"av",16},{"sv",0},{"mdb",-2},{"rdb",-2},{"eq_mov",-1},
-        {"eq_init",-3},{"eq_enc",6}
-    };
-    UDictionary<string,float> Half_Plate = new UDictionary<string, float>(){
-        {"av",14},{"sv",0},{"mdb",-1},{"rdb",-1},{"eq_mov",-1},
-        {"eq_init",-1},{"eq_enc",4}
-    };
-    UDictionary<string,float> Synthe_Armor = new UDictionary<string, float>(){
-        {"av",13},{"sv",0},{"mdb",0},{"rdb",0},{"eq_mov",0},
-        {"eq_init",0},{"eq_enc",2}
-    };
-    UDictionary<string,float> Legionary_Armor = new UDictionary<string, float>(){
-        {"av",15},{"sv",0},{"mdb",-1},{"rdb",-2},{"eq_mov",0},
-        {"eq_init",-1},{"eq_enc",4}
-    };
-    UDictionary<string,float> Praetorian_Armor = new UDictionary<string, float>(){
-        {"av",18},{"sv",0},{"mdb",1},{"rdb",-3},{"eq_mov",-1},
-        {"eq_init",-3},{"eq_enc",7}
-    };
-    UDictionary<string,float> Flak_Suit = new UDictionary<string, float>(){
-        {"av",13},{"sv",0},{"mdb",-1},{"rdb",0},{"eq_mov",0},
-        {"eq_init",0},{"eq_enc",2}
-    };
-    UDictionary<string,float> Assault_Vest = new UDictionary<string, float>(){
-        {"av",16},{"sv",0},{"mdb",0},{"rdb",-1},{"eq_mov",-1},
-        {"eq_init",-2},{"eq_enc",4}
-    };
-    UDictionary<string,float> Personal_Shield_MK_I = new UDictionary<string, float>(){
-        {"av",0},{"sv",15},{"mdb",0},{"rdb",0},{"eq_mov",0},
-        {"eq_init",0},{"eq_enc",1}
-    };
-    UDictionary<string,float> Personal_Shield_MK_II = new UDictionary<string, float>(){
-        {"av",0},{"sv",17},{"mdb",0},{"rdb",0},{"eq_mov",0},
-        {"eq_init",0},{"eq_enc",1}
-    };
-    UDictionary<string,float> Personal_Shield_MK_III = new UDictionary<string, float>(){
-        {"av",0},{"sv",19},{"mdb",-1},{"rdb",-1},{"eq_mov",0},
-        {"eq_init",-1},{"eq_enc",2}
-    };
+    UDictionary<string,float> Plate, Half_Plate, Synthe_Armor, Legionary_Armor, Praetorian_Armor, Flak_Suit,Assault_Vest, Personal_Shield_MK_I,Personal_Shield_MK_II,Personal_Shield_MK_III = new UDictionary<string,float>();
+    UDictionary<string,UDictionary<string,float>> Armor_lst = new UDictionary<string, UDictionary<string, float>>(); 
+
     public UDictionary<string,float> getArmorStat(string armor){
-        switch(armor){
-            case "Plate":
-                return Plate;
-            case "Half Plate":
-                return Half_Plate;
-            case "Synthe Armor":
-                return Synthe_Armor;
-            case "Legionary Armor":
-                return Legionary_Armor;
-            case "Praetorian_Armor":
-                return Praetorian_Armor;
-            case "Flak Suit":
-                return Flak_Suit;
-            case "Assault Vest":
-                return Assault_Vest;
-            case "Personal Shield MK I":
-                return Personal_Shield_MK_I;
-            case "Personal Shield MK II":
-                return Personal_Shield_MK_II;
-            case "Personal Shield MK III":
-                return Personal_Shield_MK_III;
+        if(Armor_lst.ContainsKey(armor)){
+            return Armor_lst[armor];
         }
         return null;
 
@@ -222,18 +213,12 @@ public class Equipments : ScriptableObject
     3. init 
     4. enc
     */
-    UDictionary<string,float> Pack_Horse = new UDictionary<string, float>(){
-        {"base_hp",30},{"mdb",3},{"eq_mov",8},{"eq_init",+5},{"eq_enc",-6}
-    };
-    UDictionary<string,float> War_Horse = new UDictionary<string, float>(){
-        {"base_hp",35},{"mdb",4},{"eq_mov",9},{"eq_init",+6},{"eq_enc",-4}
-    };
+    UDictionary<string,float> Pack_Horse,War_Horse= new UDictionary<string,float>();
+    UDictionary<string,UDictionary<string,float>> Mount_lst = new UDictionary<string, UDictionary<string, float>>();    
+
     public UDictionary<string,float> getMountStat(string mount){    
-        switch(mount){
-            case "Pack Horse":
-                return Pack_Horse;
-            case "War Horse":
-                return War_Horse;
+        if(Mount_lst.ContainsKey(mount)){
+            return Mount_lst[mount];
         }
         return null;
     }
--- Assets/Scripts/Data/Types.cs
+++ Assets/Scripts/Data/Types.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Types", order = 2)]
 public class Types:ScriptableObject
 {
-    public List<string> type = new List<string>(){"none","Praetorian Guard", "Imperial Legionary", "Imperial Conscript", "Mecenary", "Brigand"};
+    public List<string> type = new List<string>(){"none","Praetorian Guard", "Imperial Legionary", "Imperial Conscript", "Mercenary", "Brigand"};
     /*
     0. pow
     1. dex
@@ -24,43 +24,42 @@ public class Types:ScriptableObject
     14. rd
     15. mr
     */
-    UDictionary<string,float> Praetorian_Guard = new UDictionary<string, float>(){
-        {"pow",8},{"dex",6},{"tou",7},{"acu",4},{"mid",3},
-        {"base_hp",10},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
-        {"ma",15},{"ra",9},{"sa",3},{"md",16},{"rd",16},{"mr",13}
-    };
-    UDictionary<string,float> Imperial_Legionary = new UDictionary<string, float>(){
-        {"pow",7},{"dex",5},{"tou",6},{"acu",2},{"mid",3},
-        {"base_hp",10},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
-        {"ma",13},{"ra",9},{"sa",3},{"md",15},{"rd",14},{"mr",10}
-    };
-    UDictionary<string,float> Imperial_Conscript = new UDictionary<string, float>(){
-        {"pow",5},{"dex",5},{"tou",5},{"acu",1},{"mid",2},
-        {"base_hp",0},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
-        {"ma",10},{"ra",7},{"sa",3},{"md",10},{"rd",10},{"mr",10}
-    };
-    UDictionary<string,float> Mecenary = new UDictionary<string, float>(){
-        {"pow",6},{"dex",6},{"tou",5},{"acu",2},{"mid",2},
-        {"base_hp",5},{"base_ene",0},{"base_mov",1},{"base_init",11},{"base_enc",0},
-        {"ma",12},{"ra",11},{"sa",3},{"md",13},{"rd",12},{"mr",10}
-    };
-    UDictionary<string,float> Brigand = new UDictionary<string, float>(){
-        {"pow",4},{"dex",5},{"tou",4},{"acu",1},{"mid",2},
-        {"base_hp",0},{"base_ene",0},{"base_mov",2},{"base_init",10},{"base_enc",0},
-        {"ma",9},{"ra",11},{"sa",3},{"md",9},{"rd",9},{"mr",10}
-    };
+    UDictionary<string,float> Praetorian_Guard,Imperial_Legionary,Imperial_Conscript,Mercenary,Brigand = new UDictionary<string, float>();
+    UDictionary<string,UDictionary<string,float>> Type_lst = new UDictionary<string, UDictionary<string, float>>();
+    void Awake(){
+        Praetorian_Guard = new UDictionary<string, float>(){
+            {"pow",8},{"dex",6},{"tou",7},{"acu",4},{"mid",3},
+            {"base_hp",10},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
+            {"ma",15},{"ra",9},{"sa",3},{"md",16},{"rd",16},{"mr",13}
+        };
+        Imperial_Legionary = new UDictionary<string, float>(){
+            {"pow",7},{"dex",5},{"tou",6},{"acu",2},{"mid",3},
+            {"base_hp",10},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
+            {"ma",13},{"ra",9},{"sa",3},{"md",15},{"rd",14},{"mr",10}
+        };
+        Imperial_Conscript = new UDictionary<string, float>(){
+            {"pow",5},{"dex",5},{"tou",5},{"acu",1},{"mid",2},
+            {"base_hp",0},{"base_ene",0},{"base_mov",1},{"base_init",10},{"base_enc",0},
+            {"ma",10},{"ra",7},{"sa",3},{"md",10},{"rd",10},{"mr",10}
+        };
+        Mercenary = new UDictionary<string, float>(){
+            {"pow",6},{"dex",6},{"tou",5},{"acu",2},{"mid",2},
+            {"base_hp",5},{"base_ene",0},{"base_mov",1},{"base_init",11},{"base_enc",0},
+            {"ma",12},{"ra",11},{"sa",3},{"md",13},{"rd",12},{"mr",10}
+        };
+        Brigand = new UDictionary<string, float>(){
+            {"pow",4},{"dex",5},{"tou",4},{"acu",1},{"mid",2},
+            {"base_hp",0},{"base_ene",0},{"base_mov",2},{"base_init",10},{"base_enc",0},
+            {"ma",9},{"ra",11},{"sa",3},{"md",9},{"rd",9},{"mr",10}
+        };
+        Type_lst = new UDictionary<string, UDictionary<string, float>>(){
+        {"Praetorian Guard", Praetorian_Guard}, {"Imperial Legionary", Imperial_Legionary},{"Imperial Conscript",Imperial_Conscript}
+        ,{"Mercenary",Mercenary},{"Brigand",Brigand}
+        };
+    }
     public UDictionary<string,float> getTypeStat(string type){
-        switch(type){
-            case "Praetorian Guard":
-                return Praetorian_Guard;
-            case "Imperial Legionary":
-                return Imperial_Legionary;
-            case "Imperial Conscript":
-                return Imperial_Conscript;
-            case "Mecenary":
-                return Mecenary;
-            case "Brigand":
-                return Brigand;
+        if(Type_lst.ContainsKey(type)){
+            return Type_lst[type];
         }
         return null;
     }
@@ -74,7 +73,7 @@ public class Types:ScriptableObject
     public UDictionary<string,string> Imperial_Conscript_Abilities = new UDictionary<string,string>(){
         {"LeadershipAura","Universal"},{"Charge","Individual"}
     };
-    public UDictionary<string,string> Mecenary_Abilities = new UDictionary<string,string>(){
+    public UDictionary<string,string> Mercenary_Abilities = new UDictionary<string,string>(){
         {"LeadershipAura","Universal"},{"Charge","Individual"}
     };
     public UDictionary<string,string> Brigand_Abilities = new UDictionary<string,string>(){
@@ -88,8 +87,8 @@ public class Types:ScriptableObject
                 return Imperial_Legionary_Abilities;
             case "Imperial Conscript":
                 return Imperial_Conscript_Abilities;
-            case "Mecenary":
-                return Mecenary_Abilities;
+            case "Mercenary":
+                return Mercenary_Abilities;
             case "Brigand":
                 return Brigand_Abilities;
         }
